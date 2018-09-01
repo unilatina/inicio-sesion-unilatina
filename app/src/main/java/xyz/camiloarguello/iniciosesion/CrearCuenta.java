3c190102696c1f9f80646e383563dce035a50d33
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-09-01 12:31:11 -0500

    Actividad Crear Cuenta

--- app/src/main/java/xyz/camiloarguello/iniciosesion/CrearCuenta.java
+++ app/src/main/java/xyz/camiloarguello/iniciosesion/CrearCuenta.java
@@ -1,21 +1,19 @@
 package xyz.camiloarguello.iniciosesion;
 
 import android.content.Intent;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class CrearCuenta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_crear_cuenta);
     }
-
-    public void irACrearCuenta(View view){
-        Intent a = new Intent(getApplicationContext(), CrearCuenta.class );
-        startActivity(a);
+    public void irALogin(View view){
+        Intent iralogin = new Intent(this, LoginActivity.class);
+        startActivity(iralogin);
     }
 }
