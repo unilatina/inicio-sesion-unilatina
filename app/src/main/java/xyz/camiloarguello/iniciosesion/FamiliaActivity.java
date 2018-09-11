e81bde0df6ed57a3e7498ce6234cfeeeeb2e5c04
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-09-10 21:38:55 -0500

    Actividad Famili

--- app/src/main/java/xyz/camiloarguello/iniciosesion/FamiliaActivity.java
+++ app/src/main/java/xyz/camiloarguello/iniciosesion/FamiliaActivity.java
@@ -5,35 +5,41 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class FamiliaActivity extends AppCompatActivity {
 
-    ArrayList<String> myFamily;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_familia);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // Calling the listview
-        ListView listView = findViewById(R.id.myFamily);
-
-        myFamily = new ArrayList<>();
+        // Creo los datos con ArrayList
+        ArrayList<String> myFamily = new ArrayList<>();
+        myFamily.add("Jose");
+        myFamily.add("Blanca");
+        myFamily.add("Camilo");
+        myFamily.add("Edison");
+        myFamily.add("Carlos");
+        myFamily.add("Angela");
+        myFamily.add("Sophia");
+        myFamily.add("Jose");
+        myFamily.add("Blanca");
+        myFamily.add("Camilo");
+        myFamily.add("Edison");
+        myFamily.add("Carlos");
+        myFamily.add("Angela");
+        myFamily.add("Sophia");
+        myFamily.add("Jose");
+        myFamily.add("Blanca");
+        myFamily.add("Camilo");
+        myFamily.add("Edison");
+        myFamily.add("Carlos");
+        myFamily.add("Angela");
+        myFamily.add("Sophia");
         myFamily.add("Jose");
         myFamily.add("Blanca");
         myFamily.add("Camilo");
@@ -42,9 +48,12 @@ public class FamiliaActivity extends AppCompatActivity {
         myFamily.add("Angela");
         myFamily.add("Sophia");
 
-        // Convert Array into a List view with Adapter
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, myFamily);
-        listView.setAdapter(arrayAdapter);
+        // Llamo el elemento lista que definí
+        ListView listView = findViewById(R.id.familia);
+
+        // Creo el adaptador para sincronizarlo con la información
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myFamily);
+        listView.setAdapter(adaptador);
 
     }
 }
