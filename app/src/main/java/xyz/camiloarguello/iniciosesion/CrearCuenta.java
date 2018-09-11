13e4efd862324b6f4b73476b6a943db67584e68e
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-09-10 21:59:44 -0500

    Created menu

--- app/src/main/java/xyz/camiloarguello/iniciosesion/CrearCuenta.java
+++ app/src/main/java/xyz/camiloarguello/iniciosesion/CrearCuenta.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class CrearCuenta extends AppCompatActivity {
@@ -22,5 +25,24 @@ public class CrearCuenta extends AppCompatActivity {
     public void irALogin(View view){
         Intent iralogin = new Intent(this, LoginActivity.class);
         startActivity(iralogin);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemWasSelected = item.getItemId();
+
+        if(menuItemWasSelected == R.id.buscar){
+            Toast.makeText(this, "Clickeaste buscar", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Clickeaste acerca de", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
