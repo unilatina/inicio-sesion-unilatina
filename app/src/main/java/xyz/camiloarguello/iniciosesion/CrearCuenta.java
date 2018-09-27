6c35f078613a214453c377cd1b40f61b574712c7
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-09-26 22:22:59 -0500

    firebase

--- app/src/main/java/xyz/camiloarguello/iniciosesion/CrearCuenta.java
+++ app/src/main/java/xyz/camiloarguello/iniciosesion/CrearCuenta.java
@@ -1,27 +1,90 @@
 package xyz.camiloarguello.iniciosesion;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class CrearCuenta extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private Button btnCreateAccount;
+    private TextInputEditText emailTextInput;
+    private TextInputEditText nombreUsuarioTextInput;
+    private TextInputEditText usuarioTextInput;
+    private TextInputEditText passwordTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
 
+        btnCreateAccount = findViewById(R.id.btn_crear_cuenta);
+        emailTextInput = findViewById(R.id.email_create_account);
+        nombreUsuarioTextInput = findViewById(R.id.nombre_usuario);
+        usuarioTextInput = findViewById(R.id.usuario);
+        passwordTextInput = findViewById(R.id.password_crear_cuenta);
+
+        initialize();
+
         Toolbar toolbarRegistro = findViewById(R.id.miToolbar);
         setSupportActionBar(toolbarRegistro);
         getSupportActionBar().setTitle("Registro");
 
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccount(emailTextInput.getText().toString(), passwordTextInput.getText().toString());
+            }
+        });
+
     }
+    public void initialize(){
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(firebaseAuth != null){
+                    Log.i("HOLA", "Loggeado " + user.getEmail());
+                }else{
+                    Log.w("ERROR", "Cerro sesión");
+                }
+
+            }
+        };
+    }
+
+    public void createAccount(String mail, String pass){
+        auth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(CrearCuenta.this,"Cuenta creada", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(CrearCuenta.this,"ALgo salió mal", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
     public void irALogin(View view){
         Intent iralogin = new Intent(this, LoginActivity.class);
         startActivity(iralogin);
--- app/src/main/java/xyz/camiloarguello/iniciosesion/LoginActivity.java
+++ app/src/main/java/xyz/camiloarguello/iniciosesion/LoginActivity.java
@@ -2,25 +2,75 @@ package xyz.camiloarguello.iniciosesion;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initialize();
+    }
+
+    public void initialize(){
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(firebaseAuth != null){
+                    Log.i("HOLA", "Loggeado " + user.getEmail());
+                }else{
+                    Log.w("ERROR", "Cerro sesión");
+                }
+
+            }
+        };
     }
 
     public void irACrearCuenta(View view){
-        Intent a = new Intent(getApplicationContext(), CrearCuenta.class );
-        startActivity(a);
-    }
-    public void irAAudio(View view){
-        Intent a = new Intent(getApplicationContext(), AudioActivity.class );
-        startActivity(a);
+       Intent a = new Intent(getApplicationContext(), CrearCuenta.class );
+       startActivity(a);
     }
 
+
 }
+
+/**
+ *
+ * myRef.addValueEventListener(new ValueEventListener() {
+@Override
+public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+String value = dataSnapshot.getValue(String.class);
+Snackbar.make(findViewById(R.id.login_layout),"Su dato es " + value,Snackbar.LENGTH_LONG).setAction("Deshacer", null).show();
+}
+
+@Override
+public void onCancelled(@NonNull DatabaseError databaseError) {
+Log.w("ERROR", "No se obtuvo el valor", databaseError.toException());
+}
+});
+ */
